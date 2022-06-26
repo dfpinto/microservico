1e67ef9cb90528315d2ece15b2cd9f991d39e104
dfpinto <dirley.figueredo@gmail.com>
2022-06-26 19:05:33 -0300

    UserFeignClient implementation

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/Role.java
@@ -0,0 +1,54 @@
+package com.devsuperior.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String roleName;
+	
+	public Role() {
+	}
+
+	public Role(Integer id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
